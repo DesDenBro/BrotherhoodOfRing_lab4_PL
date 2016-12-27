655d04d671dc7dac73012a8c029789d5f5c873b7
Денис <@DENIS-PC>
2016-12-27 16:36:28 +0700

    Added project.

---
+++ lab4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab4
+{
+    public class Token
+    {
+        private string _data;
+        private int _recipient;
+
+        public string Data { get { return _data; } }
+        public int Recipient { get { return _recipient; } }
+
+        public Token(string data, int recipient)
+        {
+            _data = data;
+            _recipient = recipient;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewThread(new Token("Message", 20), 0);
+            Console.Read();
+        }
+
+        static void NewThread(Token token, int index)
+        {
+            Console.WriteLine(string.Format("Index of this thread is {0}", index));
+            if (index < token.Recipient)
+            {
+                Thread tmpThr = new Thread(() => NewThread(token, index + 1));
+                tmpThr.Start();
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Thread {0} is recipient, data is {1}", index, token.Data));
+            }
+            Console.WriteLine(string.Format("Thread {0} is ended", index));
+        }
+    }
+}
